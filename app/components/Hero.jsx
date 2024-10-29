92f019db2d88ac6069059583408d6f29fe242c52
Moaz Adrian <moazadrian@gmail.com>
2024-10-29 16:57:46 +0500

    Animated Hero Section

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -1,8 +1,35 @@
-import React from 'react'
+'use client'
+import React from 'react';
+import gsap from 'gsap';
+import { useGSAP } from '@gsap/react';
 
 const Hero = () => {
+  useGSAP(()=> {
+    gsap.to('#hero', {
+      opacity: 1,
+      delay: 2,
+    })
+    gsap.to('#cta', {
+      opacity: 1,
+      y: -50,
+      delay: 2,
+    })
+  }, [])
   return (
-    <div>Hero</div>
+    <section className='w-full nav-height bg-black relative'>
+      <div className="h-5/6 w-full flex-center flex-col">
+      <p id='hero' className='hero-title'>iPhone 15 Pro</p>
+      <div className="md:w-10/12 w-9/12 pointer-events-none">
+      <video className='max-sm:hidden pointer-events-none' src={require('../../public/assets/videos/hero.mp4')} autoPlay muted loop/>
+      <video className='sm:hidden' src={require('../../public/assets/videos/smallHero.mp4')} autoPlay muted loop/>
+      </div>
+      </div>
+      <div className="flex flex-col items-center opacity-0 translate-y-20" id='cta'>
+
+      <a href="#highlights" className='btn'>Purchase</a>
+      <p className='font-normal text-xl'>From $199/m or $999</p>
+      </div>
+    </section>
   )
 }
 
--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-};
 
+  
+};
+  // next.config.js
+const withVideos = require('next-videos')
+module.exports = withVideos()
 export default nextConfig;
